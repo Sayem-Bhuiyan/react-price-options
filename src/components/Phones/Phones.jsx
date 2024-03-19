7c1b57c8d2841e34e02f0577fa8a2009fa799b8a
Sayem <sayem465508@gmail.com>
2024-03-19 17:07:34 +0600

    added a loader before loading the data

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -3,17 +3,15 @@ import { useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
-  Cell,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
+  Tooltip
 } from "recharts";
+import { Audio } from 'react-loader-spinner'
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -31,17 +29,28 @@ const Phones = () => {
           return object;
         });
         setPhones(phonesWithFakeData);
+        setLoading(false)
       });
   }, []);
   console.log(phones);
   return (
     <div>
       <h2 className="text-5xl">Phones: {phones.length}</h2>
-      
+
+      {loading && <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />}
+
       <BarChart width={600} height={400} data={phones}>
-      <XAxis width={800} dataKey='name' />
-      <YAxis />
-      j<Tooltip />
+        <XAxis width={800} dataKey="name" />
+        <YAxis />
+        j<Tooltip />
         <Bar dataKey="price" fill="#8884d8" />
       </BarChart>
     </div>
