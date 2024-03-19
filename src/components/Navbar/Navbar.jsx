e0d6cb61a75aae6e66791c3989afdaa0c47415c6
Sayem <sayem465508@gmail.com>
2024-03-19 12:37:30 +0600

    responsive the navbar and added a toggle icon for small device to toggle navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   const routes = [
     { path: "/", name: "Home", id: 1 },
     { path: "/about", name: "About", id: 2 },
@@ -10,11 +14,26 @@ const Navbar = () => {
   ];
 
   return (
-    <div>
-      <ul className="md:flex">
-        {
-            routes.map(route => <Link key={route.id} route={route} />)
-        }
+    <div className="text-black bg-gray-100 p-5">
+      <div onClick={() => setOpen(!open)} className="md:hidden text-2xl">
+        {open === true ? (
+          <AiOutlineClose />
+        ) : (
+          <AiOutlineMenu />
+        )}
+      </div>
+      {/* {open && (
+        <ul className="md:flex">
+          {routes.map((route) => (
+            <Link key={route.id} route={route} />
+          ))}
+        </ul>
+      )} */}
+ 
+      <ul className={`md:flex absolute md:static duration-1000 bg-gray-100  ${open ? 'top-16': '-top-60'}`}>
+        {routes.map((route) => (
+          <Link key={route.id} route={route} />
+        ))}
       </ul>
     </div>
   );
